88f55118a6297dd0f98717d4dae626da8002470c
kawano-ryuya <kawano.ryuya@jp.panasonic.com>
2024-03-31 18:05:46 +0900

    committed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+import { basePath } from "../../next.config.js"; // 追加
+const BASE_PATH = basePath ? basePath : ""; // 追加
+
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div>
+      <Image
+        src={`${BASE_PATH}/vercel.svg`} // 修正
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
+      test
+    </div>
+  );
 }
